f20854b3236cc22c2a2360a0362ce440bcddf7d7
saturated <46495303+easir000@users.noreply.github.com>
2025-02-02 17:32:48 +0600

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,29 +1,11 @@
-# from django.urls import path
-
-from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path
 from . import views
-from .views import *
-from django.conf import settings
-
-
-
 
 urlpatterns = [
-   
-   
-   
-    
-   path('images', views.generate_image_from_text, name='images'),
-   path('home', views.home, name='dashboard'),
-   path('profile', views.profile, name='profile'),
-   path('<str:ref_code>/', views.profile, name='profile'),
-   
-   
+    path('home', views.home, name='dashboard'),
+    path('profile', views.profile, name='profile'),
+    path('<str:ref_code>/', views.profile, name='profile'),
+    path('results/', views.results, name='result'),
+    path('bookmark/<int:hotel_id>/', views.bookmark_hotel, name='bookmark_hotel'),
+    path('bookmarks/', views.bookmarked_hotels, name='bookmarked_hotels'),
 ]
-   
-   
-
-
-
